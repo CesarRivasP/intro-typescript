ef422b4eb36f5504f89722e68bf91ed1c0d82b8b
cesarrivas10 <cesarjose58@gmail.com>
2020-08-21 13:15:40 -0400

    Create types lesson

--- types.js
+++ types.js
@@ -1,3 +1,4 @@
+"use strict";
 // Anotacion de tipos
 var car;
 var quantity;
@@ -20,9 +21,45 @@ var people = {
 console.log(people.age)
 console.log(people.address) */
 // Tipo Array
-var courses = [
+var coursesArray = [
     'React',
     'Patrones de diseño',
     'MongoDB'
 ];
-console.log(courses);
+for (var _i = 0, coursesArray_1 = coursesArray; _i < coursesArray_1.length; _i++) {
+    var course = coursesArray_1[_i];
+    console.log(course);
+}
+console.log(coursesArray);
+var hobbies = [
+    "Programar",
+    10
+];
+console.log(hobbies);
+// Tipo tuples
+var hypercar = ['Ferrari', 2016, 'LaFerrari'];
+hypercar[1] = 2021;
+console.log(hypercar);
+var motocycle = ['Ducati', 2018, 'Monster'];
+// Tipo Enum
+// nos permite utilizar valores fijos
+var role;
+(function (role) {
+    role[role["student"] = 0] = "student";
+    role[role["teacher"] = 1] = "teacher";
+})(role || (role = {}));
+;
+var person = {
+    roleTeacher: role.teacher,
+    roleStudent: role.student //0
+};
+console.log(person);
+// Tipo any
+// se pueden definir distintos valores dentro de un array
+// no es muy recomendable ya que si se utiliza, se pierde el proposito de typescript para validar types
+// es recomendado usar tuples en estos casos
+var bikes = ["KTM", 2019];
+// type literal
+// el valor asignado se toma como un tipo, y este no se puede cambiar nuevamente por otro type que se quiere indicar luego
+var auto;
+auto = 'automovil';
